8a5e3bf49ccb8e0104a2280c2638ab0a192c704f
mgarbowski <mikolaj.garbowski@gmail.com>
2024-11-11 00:30:52 +0100

    add docker file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Projekt PIS 2024Z</h1>
+      <p>Profile: {process.env.NODE_ENV}</p>
       <p>Hello world</p>
       <ClickCounter/>
       {/*TODO: API call*/}
