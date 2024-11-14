ab84b31f212070812c2d62f730cafc91d31ec590
xDepcio <olek.drwal@gmail.com>
2024-11-14 22:33:22 +0100

    added script to set up infractructure

---
+++ deployment/azure/provision-infra.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Create public IP for ci cd vm
+az network public-ip create \
+    --resource-group "pis-rg" \
+    --name "ci-cd-ip"
+
+# Create public IP for deployment vm
+az network public-ip create \
+    --resource-group "pis-rg" \
+    --name "deployment-ip"
+
+# Create virtual network
+az network vnet create \
+    --resource-group "pis-rg" \
+    --name "pis-vnet" \
+    --address-prefix 10.0.0.0/16
+
+# Create network security group
+az network nsg create \
+    --resource-group "pis-rg" \
+    --name "pis-nsg"
+
+# Add network security group rule
+az network nsg rule create \
+    --resource-group "pis-rg" \
+    --nsg-name "pis-nsg" \
+    --name "pis-nsg-rule-ssh" \
+    --protocol tcp \
+    --priority 1000 \
+    --destination-port-range 22 \
+    --access allow
+
+# Create subnet
+az network vnet subnet create \
+        --resource-group "pis-rg" \
+        --vnet-name "pis-vnet" \
+        --name "pis-subnet" \
+        --address-prefix 10.0.1.0/24 \
+        --network-security-group "pis-nsg"
+
+# Create ci cd vm network interface
+az network nic create \
+    --resource-group "pis-rg" \
+    --name "ci-cd-nic" \
+    --vnet-name "pis-vnet" \
+    --subnet "pis-subnet" \
+    --network-security-group "pis-nsg" \
+    --private-ip-address "10.0.1.4" \
+    --public-ip-address "ci-cd-ip"
+
+# Create deployment vm network interface
+az network nic create \
+    --resource-group "pis-rg" \
+    --name "deployment-nic" \
+    --vnet-name "pis-vnet" \
+    --subnet "pis-subnet" \
+    --network-security-group "pis-nsg" \
+    --private-ip-address "10.0.1.5" \
+    --public-ip-address "deployment-ip"
+
+
+# Create ci cd vm
+az vm create \
+    --resource-group "pis-rg" \
+    --name "ci-cd-vm" \
+    --location "westeurope" \
+    --image "Ubuntu2404" \
+    --nics "ci-cd-nic" \
+    --admin-username "azureuser" \
+    --ssh-key-values "./ssh-keys"
+
+# Auto-shutdown ci cd vm
+az vm auto-shutdown \
+    --resource-group "pis-rg" \
+    --name "ci-cd-vm" \
+    --time 2300
+
+# Create deployment vm
+az vm create \
+    --resource-group "pis-rg" \
+    --name "deployment-vm" \
+    --location "westeurope" \
+    --image "Ubuntu2404" \
+    --nics "deployment-nic" \
+    --admin-username "azureuser" \
+    --ssh-key-values "./ssh-keys"
+
+# Auto-shutdown ci cd vm
+az vm auto-shutdown \
+    --resource-group "pis-rg" \
+    --name "deployment-vm" \
+    --time 2300
