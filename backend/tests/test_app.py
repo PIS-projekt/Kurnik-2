12b8a26aea59058568410a7fdc1b40dbf8c519d2
Maksym <bienkowski.maksym@gmail.com>
2024-11-23 12:38:53 +0100

    Revert "remove post from stages"
    This reverts commit 4d5bbfdee57b5ddb06afc3de85351ea594b692af.

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -9,7 +9,3 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
-
-
-def test_fails():
-    assert False
