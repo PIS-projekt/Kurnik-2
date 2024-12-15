b2c80a787cc3da2fd0be58dd7b692fc3dbef4124
Maksym <bienkowski.maksym@gmail.com>
2024-12-15 20:08:23 +0100

    lifespan for schema creation

--- backend/src/psi_backend/app.py
+++ backend/src/psi_backend/app.py
@@ -1,6 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from psi_backend.db import Base, engine
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    """Sets up the database schema before
+    starting the app"""
+    Base.metadata.create_all(engine)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
