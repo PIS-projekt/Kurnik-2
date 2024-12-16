2c94de9c87835f038935b79e6451ead2bbc43e11
jedrzej-grabski <grabski.dev@gmail.com>
2024-12-16 02:57:00 +0100

    Added routing for the websocket

--- backend/src/psi_backend/app.py
+++ backend/src/psi_backend/app.py
@@ -1,7 +1,9 @@
 from contextlib import asynccontextmanager
+from fastapi import Depends, FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
-from fastapi import FastAPI
 from src.psi_backend.message import close_database, create_database, engine
+from src.psi_backend.routes.ws import ws_router
 
 
 @asynccontextmanager
@@ -14,6 +16,7 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(ws_router, prefix="/ws")
 
 
 @app.get("/")
