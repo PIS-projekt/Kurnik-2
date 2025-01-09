9c4e1d88d82fd715783c50f9046dcd94ec3ce982
Jedrzej Grabski <grabski.dev@gmail.com>
2025-01-09 22:46:06 +0100

    Changed room assignment method to support User objects

--- backend/src/psi_backend/websocket_chat/room_assignment.py
+++ backend/src/psi_backend/websocket_chat/room_assignment.py
@@ -4,6 +4,7 @@ import string
 import random
 
 from src.psi_backend.database.message import Message, message_repository
+from src.psi_backend.database.user import User
 
 UserID = int
 RoomCode = str
@@ -35,7 +36,7 @@ def assign_user_to_room(room_code: RoomCode, user_websocket: WebSocketUser):
         raise RoomNotFoundError(f"Room with code {room_code} not found.")
 
 
-async def broadcast_message(room_code: RoomCode, user_id: int, message: str):
+async def broadcast_message(room_code: RoomCode, user: User, message: str):
     """Broadcast a message to all users in the room.
 
     Args:
@@ -44,12 +45,12 @@ async def broadcast_message(room_code: RoomCode, user_id: int, message: str):
         message (str): Message to broadcast.
     """
     message_repository.add_message(
-        Message(user_id=user_id, chatroom_code=room_code, contents=message)
+        Message(user_id=user.id, chatroom_code=room_code, contents=message)
     )
 
     for websocket_user in rooms[room_code]:
         await websocket_user.websocket_connection.send_text(
-            f"User[{user_id}] said: {message}"
+            f"User[{user.username}] said: {message}"
         )
 
 
