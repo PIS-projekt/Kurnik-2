e5238d58e6f8b32bce89079e45af8f3aa0b3325f
Michal Luszczek <michluszcz@gmail.com>
2025-01-10 00:00:56 +0100

    create ReactRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,32 @@
 import "./App.css";
 import {Chat} from "./components/Chat";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 export const apiBaseUrl = "http://0.0.0.0:8000";
+export const baseAppUrl = "localhost:3000/";
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Projekt PIS 2024Z</h1>
-      <p>Profile: {process.env.NODE_ENV}</p>
-      <Chat/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Chat/>}>
+          {/*<Route path={"rooms"} element={<div/>} />*/}
+          <Route path={"chat/:roomId/:userId"} element={<Chat/>} />
+          <Route path={"chat/:roomId?/:userId?"} element={<Chat/>} />
+          <Route path="main" element={
+            <div className="App">
+              <h1>Projekt PIS 2024Z</h1>
+              <p>Profile: {process.env.NODE_ENV}</p>
+              <Chat/>
+            </div>}>
+          </Route>
+
+        </Route>
+      </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
