73e8b6966ea45e0834c791a454b25e8a510f8af1
Maksym <bienkowski.maksym@gmail.com>
2025-01-10 10:52:49 +0100

    unknown protocol join room link fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,21 @@
 import "./App.css";
-import {Chat} from "./components/Chat";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Chat } from "./components/Chat";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export const apiBaseUrl = "http://0.0.0.0:8000";
-export const baseAppUrl = "localhost:3000/";
+export const baseAppUrl = "http://localhost:3000/";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"chat/:roomId/:userId"} element={<Chat/>}/>
+        <Route path={"chat/:roomId/:userId"} element={<Chat />} />
         <Route path="/" element={
           <div className="App">
             <h1>Projekt PIS 2024Z</h1>
             <p>Profile: {process.env.NODE_ENV}</p>
-            <Chat/>
+            <Chat />
           </div>}>
         </Route>
 
