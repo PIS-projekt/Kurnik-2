3f9d084c65b00ae35a77f60c4bf3342cf95be571
xDepcio <olek.drwal@gmail.com>
2025-01-12 16:18:58 +0100

    added animation to closing chat

--- frontend/src/components/ChatComp.tsx
+++ frontend/src/components/ChatComp.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable */
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Input } from './ui/input';
 import { IoSend } from "react-icons/io5";
 import { Button } from './ui/button';
 import { FaUser } from "react-icons/fa";
 import { PiCaretDownBold } from "react-icons/pi";
+import { cn } from '../lib/utils';
 
 
 interface ChatCompProps {
@@ -12,16 +13,21 @@ interface ChatCompProps {
 }
 
 const ChatComp: FC<ChatCompProps> = ({ }) => {
+    const [closed, setClosed] = useState(false);
+
     return (
         <div className='max-w-md mx-auto'>
             <div className='bg-slate-900 rounded-t-md flex items-center relative p-2'>
                 <p className='text-center font-semibold text-slate-50 flex-1'>Chat</p>
-                <Button className='absolute right-2 hover:bg-slate-800'>
-                    <PiCaretDownBold />
+                <Button
+                    onClick={() => setClosed(!closed)}
+                    className='absolute right-2 hover:bg-slate-800'
+                >
+                    <PiCaretDownBold className={cn("transition-all", { 'rotate-180': closed })} />
                 </Button>
             </div>
-            <div className='border border-gray-200 rounded-b-md border-t-0 p-2'>
-                <div className=''>
+            <div className={cn('border border-gray-200 rounded-b-md border-t-0 p-2', { 'hidden': closed })}>
+                <div className={``}>
                     <div className='flex items-start flex-col gap-1 mb-5'>
                         <div className='flex justify-center items-center gap-1 text-muted-foreground'>
                             <FaUser className='text-xs' />
