46737f4e17ae15b252393c2b2ade743fc3687a39
xDepcio <olek.drwal@gmail.com>
2025-01-12 17:49:31 +0100

    finished useChat hook

--- frontend/src/hooks/useChat.tsx
+++ frontend/src/hooks/useChat.tsx
@@ -1,33 +1,28 @@
 /* eslint-disable */
 
-import { useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
-import { useRoom } from "./useRoom";
 
-export function useChat() {
-    const [currentMessage, setCurrentMessage] = useState("");
-    const { roomCode, setRoomCode } = useRoom();
-    const [userId, setUserId] = useState<number>(1);
+interface ChatContextType {
+    messageList: Array<{ data: string }>;
+    joinRoomChat: (roomCode: string, userId: number) => void;
+    sendMessageF: (message: string) => void;
+}
+const ChatContext = createContext<ChatContextType | undefined>(undefined);
+
+export const ChatProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [socketUrl, setSocketUrl] = useState<string | null>(null);
     const [messageList, setMessageList] = useState<Array<{ data: string }>>([]);
-    const [loggedRoomCode, setLoggedRoomCode] = useState<string | null>(null);
-    const [loggedUserId, setLoggedUserId] = useState<number | null>(null);
-    const [error, setError] = useState<string | null>(null);
 
     const apiBaseUrl = "http://0.0.0.0:8000";
 
     const handleOpen = () => {
         console.log("WebSocket connection opened.");
-        setLoggedRoomCode(roomCode);
-        setLoggedUserId(userId);
-        setError(null);
     };
 
     const handleClose = () => {
         console.log("WebSocket connection closed.");
         setMessageList([]);
-        setLoggedRoomCode(null);
-        setLoggedUserId(null);
     };
 
     const handleMessage = (message: { data: string }) => {
@@ -41,11 +36,27 @@ export function useChat() {
         shouldReconnect: () => true,
     });
 
-    const handleSendMessage = () => {
-        if (currentMessage && readyState === ReadyState.OPEN) {
-            sendMessage(currentMessage);
-            setCurrentMessage("");
-        }
-    };
+    async function joinRoomChat(roomCode: string, userId: number) {
+        setSocketUrl(`${apiBaseUrl}/ws/connect/${roomCode}?user_id=${userId}`);
+    }
 
+    async function sendMessageF(message: string) {
+        if (message && readyState === ReadyState.OPEN) {
+            sendMessage(message);
+        }
+    }
+
+    return (
+        <ChatContext.Provider value={{ messageList, joinRoomChat, sendMessageF }}>
+            {children}
+        </ChatContext.Provider>
+    );
 }
+
+export const useChat = (): ChatContextType => {
+    const context = useContext(ChatContext);
+    if (!context) {
+        throw new Error("useChat must be used within a ChatProvider");
+    }
+    return context;
+};
