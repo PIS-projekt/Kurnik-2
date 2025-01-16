8a218365fb089f3732595611f07680cb2fdff8bc
xDepcio <olek.drwal@gmail.com>
2025-01-16 14:49:22 +0100

    added logout to navbar

--- frontend/src/components/GamePage.tsx
+++ frontend/src/components/GamePage.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable */
 
-import { FC } from 'react'
-import { ChatComp } from './ChatComp'
-import RoomCreateJoin from './RoomCreateJoin'
+import { FC } from 'react';
 import { Game } from "../Games/TicTacToe/Game";
+import { ChatComp } from './ChatComp';
+import RoomCreateJoin from './RoomCreateJoin';
 
 interface GamePageProps {
 
@@ -14,12 +14,10 @@ const GamePage: FC<GamePageProps> = ({ }) => {
         <div id='reg-bg' className='min-h-screen flex'>
             <div className='backdrop-blur-[2px] backdrop-brightness-90 w-full overflow-x-hidden'>
                 <div className='max-w-screen-2xl mx-auto grid grid-cols-2 min-h-screen'>
-                    {/* <div className='flex items-center justify-center pt-12'> */}
                     <div className='pt-12 flex flex-col gap-4 px-6 py-8'>
                         <RoomCreateJoin />
                         <ChatComp className='' />
                     </div>
-                    {/* </div> */}
                     <div className='bg-white shadow-lg shadow-black min-w-[100vw]'>
                         <Game />
                     </div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable */
 import { FC } from 'react'
-import { Button } from './ui/button'
 import { FaUser } from 'react-icons/fa'
-import { Navigate, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useUser } from '../hooks/useUser'
+import { Button } from './ui/button'
 
 interface NavbarProps {
 
 }
 
 const Navbar: FC<NavbarProps> = ({ }) => {
+    const { isAuthenticated, logout } = useUser();
+
     return (
         <nav className='border-b-4 shadow-lg z-20 border-b-blue-600 fixed top-0 bg-white w-full'>
             <div className="flex justify-between items-center p-4 max-w-screen-xl mx-auto">
@@ -18,12 +21,17 @@ const Navbar: FC<NavbarProps> = ({ }) => {
                     <Link to="#" className="text-base font-semibold border-b-4 border-t-4 border-white hover:border-b-blue-600 px-2">Todo</Link>
                     <Link to="#" className="text-base font-semibold border-b-4 border-t-4 border-white hover:border-b-blue-600 px-2">Todo</Link>
                 </div>
-                <Button
-                    className='hover:bg-blue-500 rounded-full bg-blue-600 text-white font-semibold text-sm px-6'
-                >
-                    <FaUser />
-                    <Link to="/login">login</Link>
-                </Button>
+                {isAuthenticated ? (<>
+                    <Button onClick={() => logout()} className='hover:bg-blue-500 rounded-full bg-blue-600 text-white font-semibold text-sm px-6'     >
+                        <FaUser />
+                        <p>logout</p>
+                    </Button>
+                </>) : (<>
+                    <Button className='hover:bg-blue-500 rounded-full bg-blue-600 text-white font-semibold text-sm px-6'    >
+                        <FaUser />
+                        <Link to="/login">login</Link>
+                    </Button>
+                </>)}
             </div>
         </nav>
     )
